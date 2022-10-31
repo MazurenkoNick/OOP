39793276e1a73950d1b01ed4ebed709df4a1942a
nikita <nikitamazurenko42@gmail.com>
2022-10-31 22:18:09 +0200

    change GetTotalRating's implementation

--- Lab2/Lab1/Program.cs
+++ Lab2/Lab1/Program.cs
@@ -2,11 +2,18 @@
 using Lab2;
 
 // default for currentRating = 100
-GameAccount user1 = new PremiumAccount("player1", 1001);
-GameAccount user2 = new PremiumAccount("player2", 1001);
+GameAccount user1 = new BoostedAccount("player1", 1001);
+GameAccount user2 = new BoostedAccount("player2", 1001);
 
 
- user1.LoseGame(user2, new Game(100));
+ user1.WinGame(user2, new Game(100));
+user1.WinGame(user2, new Game(100));
+user1.WinGame(user2, new Game(100));
+user1.WinGame(user2, new Game(100));
+user1.LoseGame(user2, new Game(100));
+user1.WinGame(user2, new Game(100));
+user1.WinGame(user2, new Game(100));
+
 // user2.LoseGame(user1, new Game(25));
 // user1.WinGame(user2, new Game(73));
 
--- Lab2/Lab1/accounts/BoostedAccount.cs
+++ Lab2/Lab1/accounts/BoostedAccount.cs
@@ -13,42 +13,41 @@ namespace Lab1.accounts
         {
         }
 
-        private bool HasWinStrickOf(int number)
+        private int getWinStrick()
         {
             int totalGames = games.Count;
-            if (totalGames < number)
-                return false;
-
             int counter = 0;
-            while (counter < number)
+
+            while (counter < totalGames)
             {
-                // iterate over "number" last games of the player.
+                // iterate over all games of the player.
                 // if player won every single one of these games, we return true. return false otherwise 
                 if (games[totalGames-counter-1].Players[userName] == Status.Win) {
                     counter++;
                     continue;
                 }
-                return false;
+                break;
             }
-            return true;
+            return counter;
         }
 
         private double GetTotalRating(double rating)
         {
             double totalRating;
-            if (HasWinStrickOf(20)) {
+            int winStrick = getWinStrick();
+            if (winStrick >= 20) {
                 // plus 50% of the bet if player has at least 20 wins in a row
                 totalRating = rating + (rating * 0.5);
             }
-            else if (HasWinStrickOf(15)) {
+            else if (winStrick >= 15) {
                 // plus 30% of the bet if player has at least 15 wins in a row
                 totalRating = rating + (rating * 0.3);
             }
-            else if (HasWinStrickOf(10)) {
+            else if (winStrick >= 10) {
                 // plus 20% of the bet if player has at least 10 wins in a row
                 totalRating = rating + (rating * 0.2);
             }
-            else if (HasWinStrickOf(5)) {
+            else if (winStrick >= 5) {
                 // plus 10% of the bet if player has at least 5 wins in a row
                 totalRating = rating + (rating * 0.1);
             }
